c223057f77ca0ce8ab23cf13a2fa8c2c1e08211f
Stas Mykhailenko <stas.mykhailenko@gmail.com>
2018-05-07 10:16:33 +0300

    test1

--- src/app/core/services/invoice.service.ts
+++ src/app/core/services/invoice.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {HttpHeaders} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 import {Observable} from 'rxjs/Observable';
 import {ConnectableObservable} from 'rxjs/observable/ConnectableObservable';
