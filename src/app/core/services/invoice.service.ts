7c8c09f089b11387d0f834e158913803ce50e58c
Stas Mykhailenko <stas.mykhailenko@gmail.com>
2018-05-07 10:14:47 +0300

    test

--- src/app/core/services/invoice.service.ts
+++ src/app/core/services/invoice.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {HttpHeaders} from '@angular/common/http';
 
@@ -9,7 +9,7 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/publishReplay';
 import 'rxjs/add/operator/filter';
 
-import {StateManagement, StateRequests} from '../../shared/state-management';
+import { StateManagement, StateRequests } from '../../shared/state-management';
 
 import { Invoice } from '../interfaces/invoice';
 import { Action } from '../interfaces/action';
