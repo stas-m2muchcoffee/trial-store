a5e6821c7940a2315efd626d07685df1caeb1d14
Stas Mykhailenko <stas.mykhailenko@gmail.com>
2018-05-18 13:22:32 +0300

    refactor(ngrx/app-state) add field productsRequests

--- src/app/ngrx/app-state/app-state.ts
+++ src/app/ngrx/app-state/app-state.ts
@@ -1,5 +1,7 @@
 import { ProductsState } from '../products/states';
+import { IProductsRequestsState } from '../products-requests/states';
 
 export interface AppState {
   readonly products: ProductsState;
+  readonly productsRequests: IProductsRequestsState;
 }
