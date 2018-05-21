f837d3661d228604977a4007dded46d6cf77833b
Stas Mykhailenko <stas.mykhailenko@gmail.com>
2018-05-21 12:50:56 +0300

    refactor(ngrx/app-state) update AppState and reducers, delete AppReducer

--- src/app/ngrx/app-state/app-state.ts
+++ src/app/ngrx/app-state/app-state.ts
@@ -1,18 +1,14 @@
-import { combineReducers } from '@ngrx/store';
-
 import { IProductsState } from '../products/states';
 import { productsReducer } from '../products/reducers';
-import { IRequestsNestedState } from '../requests/states';
+import { IRequestsState } from '../requests/states';
 import { requestsReducer } from '../requests/reducers';
 
 export interface AppState {
   readonly products: IProductsState;
-  readonly requests: IRequestsNestedState;
+  readonly requests: IRequestsState;
 }
 
 export const reducers = {
   products: productsReducer,
   requests: requestsReducer,
 };
-
-export const AppReducer = combineReducers(reducers);
