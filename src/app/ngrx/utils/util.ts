c4419c0af8e09f217da2f0b2bf0b68a7bf1a094b
Stas Mykhailenko <stas.mykhailenko@gmail.com>
2018-05-24 19:07:55 +0300

    feat(ngrx/customers/reducers/customers.reducer) add methods getIdsArrEntities, setEntities, updateEntities

--- src/app/ngrx/utils/util.ts
+++ src/app/ngrx/utils/util.ts
@@ -11,3 +11,37 @@ export function type<T>(label: T | ''): T {
 
   return <T>label;
 }
+
+export function getIdsArrEntities(entities: any) {
+  return _.map(entities, (entity: any) => entity.id);
+}
+
+export function setEntities(entities, entityToSet) {
+  // for games
+  const oldText = entities[entityToSet.id] ? entities[entityToSet.id].text : null;
+  const newText = entityToSet.text ? entityToSet.text : null;
+  let text = {
+    text: Object.assign({}, oldText, newText)
+  };
+  text = oldText || newText ? text : null;
+
+  entityToSet = Object.assign({}, entities[entityToSet.id], entityToSet, text);
+  return _.set(
+    _.omit(entities, entityToSet.id),
+    entityToSet.id,
+    entityToSet
+  );
+}
+
+export function updateEntities(payload, entityModel?) {
+  if (!_.isArray(payload)) {
+    payload = [payload];
+  }
+  const entitiesIds = payload.map(_payload => _payload.id);
+  const entities = payload.reduce((_entities: { [id: string]: any }, entity: any) => {
+    return Object.assign(_entities, {
+      [entity.id]: entityModel ? new entityModel(entity) : entity
+    });
+  }, {});
+  return {entitiesIds, entities};
+}
